cd52c7c4bedd12ccf3d820b06b695f3a15c43952
Pury <sondongxuan1205@gmail.com>
2024-10-07 10:03:35 +0700

    first commit

---
+++ Assets/Scripts/Ship/ShipMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector3 worldPosition;
+
+    [SerializeField]
+    protected float speed = 0.1f;
+
+    private void FixedUpdate()
+    {
+        worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPosition.z = 0;
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, worldPosition, speed); // Position của ship khi di chuyển đến điểm đích
+        transform.position = newPosition;
+    }
+}
